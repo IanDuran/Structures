4acdfe9818e015667a7989db90152054a21f355c
Ian Duran <ianduran60@gmail.com>
2016-11-21 20:03:35 -0600

    Adjacency Matrix methods done except for iterator

--- src/ucr/ac/cr/ecci/ci1221/util/graph/AdjacencyMatrix.java
+++ src/ucr/ac/cr/ecci/ci1221/util/graph/AdjacencyMatrix.java
@@ -11,7 +11,8 @@ import ucr.ac.cr.ecci.ci1221.util.collections.list.List;
 public class AdjacencyMatrix<V> implements Graph<V>{
 
     private final int INITIAL_SIZE = 10;
-    private int[][] matrix;
+    private final int ENLARGING_SIZE = 5;
+    private double[][] matrix;
     private V[] values;
     private boolean directed;
     private int vertexes;
@@ -19,7 +20,7 @@ public class AdjacencyMatrix<V> implements Graph<V>{
 
     public AdjacencyMatrix(boolean directed){
         this.directed = directed;
-        matrix = new int[INITIAL_SIZE][INITIAL_SIZE];
+        matrix = new double[INITIAL_SIZE][INITIAL_SIZE];
         values = (V[]) new Object[INITIAL_SIZE];
         vertexes = 0;
         edges = 0;
@@ -32,52 +33,108 @@ public class AdjacencyMatrix<V> implements Graph<V>{
 
     @Override
     public void addNode(V value) {
+        if(vertexes == values.length)
+            this.enlarge();
 
+        values[vertexes] = value;
+        vertexes++;
     }
 
     @Override
     public void addNodes(V value1, V value2) {
-
+        this.addNode(value1);
+        this.addNode(value2);
+        this.addEdge(value1, value2);
     }
 
     @Override
     public void addEdge(V value1, V value2) {
+        if(this.contains(value1) && this.contains(value2)){
+            int firstIndex = this.getIndex(value1);
+            int secondIndex = this.getIndex(value2);
+            matrix[firstIndex][secondIndex] = 1;
+            if(!this.isDirected())
+                matrix[secondIndex][firstIndex] = 1;
 
+            edges++;
+        }
     }
 
     @Override
     public void addEdge(V value1, V value2, double weight) {
+        if(this.contains(value1) && this.contains(value2)){
+            int firstIndex = this.getIndex(value1);
+            int secondIndex = this.getIndex(value2);
+            matrix[firstIndex][secondIndex] = weight;
+            if(!this.isDirected())
+                matrix[secondIndex][firstIndex] = weight;
 
+            edges++;
+        }
     }
 
     @Override
     public boolean contains(V value) {
-        return false;
+        return this.getIndex(value) != -1;
     }
 
     @Override
     public boolean areLinked(V value1, V value2) {
-        return false;
+        boolean areLinked = false;
+        if(this.contains(value1) && this.contains(value2)){
+            int firstIndex = this.getIndex(value1);
+            int secondIndex = this.getIndex(value2);
+            if(matrix[firstIndex][secondIndex] != 0)
+                areLinked = true;
+
+        }
+        return areLinked;
     }
 
     @Override
     public double getWeight(V value1, V value2) {
-        return 0;
+        double weight = 0;
+        if(this.contains(value1) && this.contains(value2)){
+            int firstIndex = this.getIndex(value1);
+            int secondIndex = this.getIndex(value2);
+            weight = matrix[firstIndex][secondIndex];
+        }
+        return weight;
     }
 
     @Override
     public void removeValue(V value) {
-
+        if(this.contains(value)) {
+            this.moveDown(this.getIndex(value));
+            vertexes--;
+        }
     }
 
     @Override
     public void removeEdge(V value1, V value2) {
+        if(this.contains(value1) && this.contains(value2) && this.areLinked(value1, value2)){
+            int firstIndex = this.getIndex(value1);
+            int secondIndex = this.getIndex(value2);
+            matrix[firstIndex][secondIndex] = 0;
+            if(!this.isDirected())
+                matrix[secondIndex][firstIndex] = 0;
 
+            edges--;
+        }
     }
 
     @Override
     public List<V> getAdjacentNodes(V value) {
-        return null;
+        List<V> adjacentNodes = new LinkedList<>();
+        if(this.contains(value)){
+            int index = this.getIndex(value);
+            for(int i = 0; i < vertexes; i++){
+                if(matrix[index][i] != 0)
+                    adjacentNodes.add(values[i]);
+
+            }
+        }
+        return adjacentNodes;
     }
 
     @Override
@@ -130,7 +187,7 @@ public class AdjacencyMatrix<V> implements Graph<V>{
 
     @Override
     public void clear() {
-        matrix = new int[INITIAL_SIZE][INITIAL_SIZE];
+        matrix = new double[INITIAL_SIZE][INITIAL_SIZE];
         values = (V[]) new Object[INITIAL_SIZE];
         vertexes = 0;
         edges = 0;
@@ -146,5 +203,27 @@ public class AdjacencyMatrix<V> implements Graph<V>{
             counter++;
         }
         return index;
+    }
+
+    private void enlarge(){
+        V[] newValues = (V[]) new Object[values.length + ENLARGING_SIZE];
+        for(int i = 0; i < values.length; i++)
+            newValues[i] = values[i];
+
+        double[][] newMatrix = new double[matrix.length + ENLARGING_SIZE][matrix.length + ENLARGING_SIZE];
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix.length; j++){
+                newMatrix[i][j] = matrix[i][j];
+            }
+        }
+        values = newValues;
+        matrix = newMatrix;
+    }
+
+    private void moveDown(int index){
+        for(int i = index; i < vertexes - 1; i++){
+            values[i] = values[i + 1];
+            matrix[i] = matrix[i + 1];
+        }
     }
 }
